6fc2fe7d8b507e69fba8496dc6461351dd5934d2
kchaou-fakhri <kchaoufakhri@gmail.com>
2023-02-05 18:51:32 +0100

    first commit

---
+++ src/test/java/com/dev0kch/mybook/MyBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev0kch.mybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
