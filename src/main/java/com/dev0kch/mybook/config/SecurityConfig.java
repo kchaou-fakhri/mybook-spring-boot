9359aee44befe0fdfaadecd4a93a9ce3ae86f6da
kchaou-fakhri <kchaoufakhri@gmail.com>
2023-02-06 17:03:54 +0100

    bugfix(): insert book - insert book into database. feat #feature/setup_jwt_auth

--- src/main/java/com/dev0kch/mybook/config/SecurityConfig.java
+++ src/main/java/com/dev0kch/mybook/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.dev0kch.mybook.config;
 
+import com.dev0kch.mybook.jwtfilter.JwtFilter;
 import com.dev0kch.mybook.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +25,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService detailsService;
+
+    @Autowired
+    JwtFilter jwtFilter;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
        auth.userDetailsService(detailsService);
@@ -44,10 +49,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
-                .permitAll().anyRequest().authenticated();
-//                .and().exceptionHandling().and().sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
+                .permitAll().anyRequest().authenticated()
+                .and().exceptionHandling().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
     }
 
 
--- src/main/java/com/dev0kch/mybook/repository/BookRepository.java
+++ src/main/java/com/dev0kch/mybook/repository/BookRepository.java
@@ -3,10 +3,16 @@ package com.dev0kch.mybook.repository;
 import com.dev0kch.mybook.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
     @Query(value = "Select * FROM books  WHERE id = ?1", nativeQuery = true)
-    Book _findById(Long id);
+    Book _findById(int id);
+
+
+
+
 
 }
