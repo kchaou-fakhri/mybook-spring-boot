a20faf867c47795e5c9a5a6f024e5c1fa2c72b95
kchaou-fakhri <kchaoufakhri@gmail.com>
2023-02-11 22:27:53 +0100

    feat(): Change Validate token - Change Validate token from 30 hours to 1 hours for test. feat : #feature/validate_token

--- src/main/java/com/dev0kch/mybook/util/JwtUtil.java
+++ src/main/java/com/dev0kch/mybook/util/JwtUtil.java
@@ -43,7 +43,7 @@ public class JwtUtil {
     private String createToken(Map<String, Object> claims, String subject) {
 
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24*30))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(SignatureAlgorithm.HS256, secret).compact();
     }
 
