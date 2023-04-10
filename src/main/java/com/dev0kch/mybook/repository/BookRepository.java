7b3c9931e57a6a538b49cc1ad9cae19962b0ba5e
kchaou-fakhri <kchaoufakhri@gmail.com>
2023-04-10 09:47:43 +0100

    feat() : Filter by price - add attribute price to Filter class - add price param to api filter book - add group by function to book query feat #feature/filter_books

--- src/main/java/com/dev0kch/mybook/repository/BookRepository.java
+++ src/main/java/com/dev0kch/mybook/repository/BookRepository.java
@@ -17,18 +17,22 @@ public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
             "ON book.id = t2.book_id " +
             "INNER JOIN review re ON re.book_id = book.id WHERE t2.category_id in ?1" +
             " AND book.language in ?2" +
+            " AND book.price >= ?4" +
+            " GROUP BY book.id" +
             " HAVING AVG(re.value) >= ?3 ", nativeQuery = true)
     ArrayList<Book> findAllBookByCategoriesAndReviewAndLanguages( List<Long> categories,
                                              List<String> languages,
-                                             int value);
+                                             int value, int price);
 
     @Query(value = "SELECT DISTINCT book.* FROM books book INNER JOIN book_categories t2 " +
             "ON book.id = t2.book_id " +
             "WHERE t2.category_id in ?1" +
-            " AND book.language in ?2",
+            " AND book.price >= ?3" +
+            " AND book.language in ?2" +
+            " GROUP BY book.id" ,
             nativeQuery = true)
     ArrayList<Book> findAllBookByCategories( List<Long> categories,
-                                                                  List<String> languages
+                                                                  List<String> languages, int price
                                                                   );
 
 
